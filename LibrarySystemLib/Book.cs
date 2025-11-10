c3b08a24f4e1cbaa48a65e45375d3893993fd7e1
LEGION <LEGION@JELLO>
2025-11-10 21:42:38 +0800

    Add project files.

---
+++ LibrarySystemLib/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystemLib
+{
+    public class Book
+    {
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Quantity { get; set; }
+        public string Status { get; set; }
+
+    }
+}
